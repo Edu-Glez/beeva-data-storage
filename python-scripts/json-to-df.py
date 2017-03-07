ea219fd19a1e7454cd6255175dea47c16934e369
Edu-Glez <laloglezvaz@gmail.com>
2017-03-07 10:07:39 -0600

    adding script for merge

--- python-scripts/json-to-df.py
+++ python-scripts/json-to-df.py
@@ -4,8 +4,14 @@ import json
 with open('/home/graduate/data-graduates/Introduction_01/data/countries.json') as data_file:
 	data = json.load(data_file)
 
-keys = data[0].keys()
-df = pd.DataFrame(columns = keys)
 
-for i in data:
-	print(i.
+dfp=pd.DataFrame(data)
+dfaux=dfp
+
+for i in range(0, len(data)):
+	#dfp['name'][i]=dfaux['name'][i]['common']
+	dfp['name'][i]=dfaux.loc[:,('name',i)]
+dfcsv=pd.read_csv('/home/graduate/data-graduates/Introduction_01/data/countries.csv')
+
+merged=pd.merge(dfp,dfcsv,on='name')
+print(merged)
